7b2233cb031308b5541bf6fcb4c4b6da11237f73
Jon Martini <39231017+jsmartini@users.noreply.github.com>
2021-01-25 12:13:53 -0600

    concurrency

--- test/c.go
+++ test/c.go
@@ -23,8 +23,8 @@ type network interface {
 	handshake() error
 	server()
 	client()
-	send(string) error
-	recv() (string, error)
+	send(string)
+	recv()
 }
 
 func (c client) server() {
@@ -41,7 +41,13 @@ func (c client) server() {
 }
 func (c client) client() {
 	var conn net.Conn
-	conn, c.errc = net.DialTimeout("tcp", fmt.Sprintf("%s:%s", c.hostname, c.port), time.Duration(120)*time.Second)
+	for i := 0; i <= 500; i++ {
+		conn, c.errc = net.DialTimeout("tcp", fmt.Sprintf("%s:%s", c.hostname, c.port), time.Duration(300)*time.Millisecond)
+		if c.errc != nil {
+			fmt.Printf("\rTry: %d", i)
+		}
+	}
+	fmt.Println("Connected!")
 	if c.errc != nil {
 		return
 	}
@@ -52,31 +58,33 @@ func (c client) handshake() error {
 	go c.client()
 	go c.server()
 	if c.errs != nil || c.errc != nil {
-		fmt.Println("Errors:\n:\tServer: %s\n:\tClient: %s", c.errs, c.errc)
-		os.Exit(-1)
+		return fmt.Errorf("Errors:\n:\tServer:\t%s\n:\tClient:\t%s\n", c.errs, c.errc)
 	}
-	fmt.Println("Connected to %s:%s Successfully", c.hostname, c.port)
 	c.IOrw = bufio.NewReadWriter(c.IOr, c.IOw)
 	return nil
 }
 
-func (c client) send(msg string) error {
+func (c client) send() {
+	var msg string
+	fmt.Scanln("[?>", msg)
+	if msg == "exit" {
+		os.Exit(0)
+	}
 	n, err := c.IOrw.WriteString(msg + "\n")
 	if err != nil {
-		return err
+		return
 	}
 	fmt.Println("Wrote %d bytes to target Successfully", n)
-	return nil
+	return
 }
 
-func (c client) recv() (string, error) {
-	var msg string
-	var err error
-	msg, err = c.IOrw.ReadString('\n')
+func (c client) recv() {
+	msg, err := c.IOrw.ReadString('\n')
 	if err != nil {
-		return fmt.Sprintf("%s", err), err
+		return
 	}
-	return msg, nil
+	fmt.Println("[%s:%s> %s", c.hostname, c.port, msg)
+	return
 }
 
 func main() {
@@ -85,10 +93,7 @@ func main() {
 		fmt.Println("Usage:\t ./tcp [hostname] [port]")
 		os.Exit(0)
 	}
-	fmt.Println()
 	var target client
-	exec := true
-	buffer := ""
 	target.hostname = args[1]
 	target.port = args[2]
 	err := target.handshake()
@@ -96,20 +101,8 @@ func main() {
 		fmt.Println("Exiting:\t%s", err)
 		os.Exit(-1)
 	}
-	for exec {
-		fmt.Sscanln("[?>", &buffer)
-		if buffer == "exit" {
-			target.send("BYE!")
-			exec = false
-			continue
-		}
-		target.send(buffer)
-		buffer = ""
-		res, err := target.recv()
-		if err != nil {
-			continue
-		} else {
-			fmt.Println("[%s:%s> %s", target.hostname, target.port, res)
-		}
+	for {
+		go target.recv()
+		go target.send()
 	}
 }
